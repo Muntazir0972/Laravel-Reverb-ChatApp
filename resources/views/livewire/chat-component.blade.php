be09cf4d4af224f0889025e0f84fa35f19de860c
Muntazir0972 <mohammadmuntazir18@gmail.com>
2024-10-29 15:41:10 +0500

    change in form

--- resources/views/livewire/chat-component.blade.php
+++ resources/views/livewire/chat-component.blade.php
@@ -21,12 +21,14 @@
             @foreach ($messages as $message)
                 @if ($message['sender'] != auth()->user()->name)
                     <div class="clearfix w-4/4">
-                        <div class="bg-gray-300 mx-4 my-2 p-2 rounded-lg inline-block"><b>{{ $message['sender'] }} : </b>{{ $message['message'] }}</div>
+                        <div class="bg-gray-300 mx-4 my-2 p-2 rounded-lg inline-block"><b>{{ $message['sender'] }} :
+                            </b>{{ $message['message'] }}</div>
                     </div>
                 @else
                     <div class="clearfix w-4/4">
                         <div class="text-right">
-                            <p class="bg-green-300 mx-4 my-2 p-2 rounded-lg inline-block">{{ $message['message'] }} <b>: You</b></p>
+                            <p class="bg-green-300 mx-4 my-2 p-2 rounded-lg inline-block">{{ $message['message'] }} <b>:
+                                    You</b></p>
                         </div>
                     </div>
                 @endif
@@ -34,7 +36,7 @@
         </div>
     </div>
 
-    <form action="" wire:submit="sendMessage()">
+    {{-- <form action="" wire:submit="sendMessage()">
         <div class="fixed w-full flex justify-between bg-green-100" style="bottom: 0px;">
             <textarea class="flex-grow m-2 py-2 px-4 mr-1 rounded-full border border-gray-300 bg-gray-200 resize-none"
                 rows="1" wire:model="message" placeholder="Message..." style="outline: none;"></textarea>
@@ -48,5 +50,44 @@
             </button>
         </div>
 
+    </form> --}}
+
+    {{-- <form wire:submit.prevent="sendMessage">
+        <div class="fixed w-full flex justify-between bg-green-100" style="bottom: 0px;">
+            <textarea
+                class="flex-grow m-2 py-2 px-4 mr-1 rounded-full border border-gray-300 bg-gray-200 resize-none"
+                rows="1"
+                wire:model="message"
+                placeholder="Message..."
+                style="outline: none;"
+                onkeydown="if(event.key === 'Enter' && !event.shiftKey) { event.preventDefault(); this.form.submit(); }"
+            ></textarea>
+            <button type="submit" class="m-2" style="outline: none;">
+                <svg class="svg-inline--fa text-green-400 fa-paper-plane fa-w-16 w-12 h-12 py-2 mr-2" aria-hidden="true"
+                    focusable="false" data-prefix="fas" data-icon="paper-plane" role="img"
+                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path fill="currentColor"
+                        d="M476 3.2L12.5 270.6c-18.1 10.4-15.8 35.6 2.2 43.2L121 358.4l287.3-253.2c5.5-4.9 13.3 2.6 8.6 8.3L176 407v80.5c0 23.6 28.5 32.9 42.5 15.8L282 426l124.6 52.2c14.2 6 30.4-2.9 33-18.2l72-432C515 7.8 493.3-6.8 476 3.2z"
+                    />
+                </svg>
+            </button>
+        </div>
+    </form> --}}
+
+    <form wire:submit.prevent="sendMessage">
+        <div class="fixed w-full flex justify-between bg-green-100" style="bottom: 0px;">
+            <textarea class="flex-grow m-2 py-2 px-4 mr-1 rounded-full border border-gray-300 bg-gray-200 resize-none"
+                rows="1" wire:model="message" placeholder="Message..." style="outline: none;"
+                onkeydown="if(event.key === 'Enter' && !event.shiftKey) { event.preventDefault(); document.querySelector('form').dispatchEvent(new Event('submit')); }"></textarea>
+            <button type="submit" class="m-2" style="outline: none;">
+                <svg class="svg-inline--fa text-green-400 fa-paper-plane fa-w-16 w-12 h-12 py-2 mr-2" aria-hidden="true"
+                    focusable="false" data-prefix="fas" data-icon="paper-plane" role="img"
+                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path fill="currentColor"
+                        d="M476 3.2L12.5 270.6c-18.1 10.4-15.8 35.6 2.2 43.2L121 358.4l287.3-253.2c5.5-4.9 13.3 2.6 8.6 8.3L176 407v80.5c0 23.6 28.5 32.9 42.5 15.8L282 426l124.6 52.2c14.2 6 30.4-2.9 33-18.2l72-432C515 7.8 493.3-6.8 476 3.2z" />
+                </svg>
+            </button>
+        </div>
     </form>
+
 </main>
